8f8d0ecad2227e380a0c6d26cead38d334f21f9b
ameliasapputr <47627600+ameliasapputr@users.noreply.github.com>
2019-03-01 19:08:33 +0700

    Create point of sale.cpp
    point of sale dengan dev c++

---
+++ point of sale.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+#include<conio.h>
+#include <string> 
+   using namespace std;
+   int main() { 
+      int no,tgl,ns;
+      string jl,JL,tanggal,kl,ps,nm,name,nama,telp;
+   
+   
+  
+  									cout<<"IT Service "<<endl;
+  								cout<<"ACP SMKN $ PADALARANG "<<endl;
+  								cout<<"Jl. Raya Padalarang 451"<<endl;
+				cout<<"=========================================================="<<endl;
+cout<<"No =";		
+cin>>no;				
+				
+cout<<"Jenis Laptop ="; 
+cin>>jl>>JL; 
+cout<<"Tanggal =";
+ cin>>tgl>>tanggal; 
+ cout<<"nama =";
+ cin>>nm>>name;
+ cout<<"No.seri=";
+ cin>>ns;
+ cout<<"Telepon=";
+ cin>>telp;
+ 	
+
+	
+	cout<<" From Keluhan"<<endl;
+			cout<<"Keluhan Laptop ="<<endl;
+			cin>>kl;
+			cout<<"Perkiraan Selesai ="<<endl;
+			cin>>ps;
+			
+			system ("PAUSE");
+			return 0;
+}
